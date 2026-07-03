7279256221b755954fc6674fe791848882bb97f7
allanmiller143 <millerallan17@gmail.com>
2026-07-03 09:08:01 -0300

    ajuste no path do streamlite

--- app_streamlit.py
+++ app_streamlit.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="EsqueMapa", layout="wide")
 
 
 ROOT_DIR = Path(__file__).resolve().parent
-DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = ROOT_DIR / "data" / "teixeira"
 APP_DIR = ROOT_DIR / "app"
 
 
